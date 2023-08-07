f762120ce842ef9ac8d17c3eee27fcf683b14347
Stalin Salgado <superldu91@gmail.com>
2023-08-06 20:49:37 -0500

    Implementacion de rutas padres hijas y nietas

---
+++ src/app/auth/pages/layout-page/layout-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './layout-page.component.html',
+  styleUrls: ['./layout-page.component.css']
+})
+export class LayoutPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
